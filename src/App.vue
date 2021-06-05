a139175f896658c19e006fd683533ca6c9c39e5f
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-06-05 10:57:45 +0700

    resolve error detection from vetur

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,126 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h3>Parsing data</h3>
+  <div class="button">
+    <button @click="page++">incress page</button>
+    <button @click="page--">decress page</button>
+    <button @click="togleBook">Hide book</button>
+    <button @click="togleFilterFav">Filter Fav</button>: {{filter ? "Favorite book" : "All"}}
+  </div>
+  <span v-bind:title="message" v-if="showBook">
+    <a :href="instagram">Cek my profil</a>
+    <div>
+        pages: {{ page }}
+    </div>
+    <ul class="list-book">
+        <li v-for="(book, i) in filter ? filteredBooks : books" v-bind:key="i" v-bind:class="{ fav: book.fav}" @click="togleFav(i)">
+          <img :src="book.img" :alt="book.title">
+          <div class="content">
+              <h3>{{book.title}}</h3>
+              <p>Author: {{book.author}}</p>
+          </div>
+        </li>
+    </ul>
+  </span>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      showBook: true,
+      filter: true,
+      instagram: "https://instagram.com/abam.afriansyah",
+      message: 'You loaded this page on ' + new Date().toLocaleString(),
+      books: [
+        {
+            title: "Bincang Akhlak",
+            author: "Ustad Jek",
+            img: "/assets/1.jpg",
+            fav: true,
+        },
+        {
+            title: "Ubur Ubur Lembur",
+            author: "Raditya Dika",
+            img: "/assets/2.jpg",
+            fav: false,
+        },
+        {
+            title: "This is Why I Need You",
+            author: "Brian Khrisna",
+            img: "/assets/3.jpg",
+            fav: true,
+        },
+      ],
+      page: 321,
+    }
+  },
+  methods: {
+    reverseHandling() {
+        this.page++
+    },
+    togleBook() {
+        this.showBook = !this.showBook
+    },
+    togleFav(e) {
+        this.books[e].fav = !this.books[e].fav
+    },
+    togleFilterFav() {
+        this.filter = !this.filter
+    }
+  },
+  computed: {
+    filteredBooks() {
+        return this.books.filter((book) => book.fav)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .container {
+    margin: 1rem;
+    padding: 1rem;
+    border: solid 1px;
+    border-radius: 10px;
+  }
+
+  #data span {
+    display: block;
+  }
+
+  .box {
+    text-align: center;
+    background: #ddd;
+    width: 200px;
+    padding: 100px 0;
+    margin: 20px;
+    display: inline-block;
+  }
+
+  .list-book {
+    list-style: none;
+    padding: 0;
+    width: auto;
+  }
+
+  .list-book li {
+    background-color: lightgrey;
+    margin: 1rem;
+    padding: 1rem;
+    /* border: solid 1px black; */
+    border-radius: 10px;
+    box-shadow: 5px 5px 5px #ddd;
+    display: flex ;
+  }
+
+  .list-book li.fav {
+    color: midnightblue;
+    background-color: lightpink;
+  }
+
+
+  .list-book li img {
+    padding: 0px 1rem;
+  }
 </style>
