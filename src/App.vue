9db5acdc0ccb6abb48a3bb80f515cb273b043fb2
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-06-05 11:30:26 +0700

    add modal component

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,17 @@
         </li>
     </ul>
   </span>
+  <Modal />
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
+  components: {
+    Modal,
+  },
   data() {
     return {
       showBook: true,
