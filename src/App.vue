8352406fa2272f4ca2b8f9c0e4afe61f51c67764
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-06-05 12:09:46 +0700

    props data

--- src/App.vue
+++ src/App.vue
@@ -4,129 +4,140 @@
     <button @click="page++">incress page</button>
     <button @click="page--">decress page</button>
     <button @click="togleBook">Hide book</button>
-    <button @click="togleFilterFav">Filter Fav</button>: {{filter ? "Favorite book" : "All"}}
+    <button @click="togleFilterFav">Filter Fav</button>:
+    {{ filter ? "Favorite book" : "All" }}
   </div>
   <span v-bind:title="message" v-if="showBook">
     <a :href="instagram">Cek my profil</a>
-    <div>
-        pages: {{ page }}
-    </div>
+    <div>pages: {{ page }}</div>
     <ul class="list-book">
-        <li v-for="(book, i) in filter ? filteredBooks : books" v-bind:key="i" v-bind:class="{ fav: book.fav}" @click="togleFav(i)">
-          <img :src="book.img" :alt="book.title">
-          <div class="content">
-              <h3>{{book.title}}</h3>
-              <p>Author: {{book.author}}</p>
-          </div>
-        </li>
+      <li
+        v-for="(book, i) in filter ? filteredBooks : books"
+        v-bind:key="i"
+        v-bind:class="{ fav: book.fav }"
+        @click="togleFav(i)"
+      >
+        <img :src="book.img" :alt="book.title" />
+        <div class="content">
+          <h3>{{ book.title }}</h3>
+          <p>Author: {{ book.author }}</p>
+        </div>
+      </li>
     </ul>
   </span>
-  <Modal />
+  <Modal :modalData="modal" :text="modal.text" />
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Modal,
+    Modal
   },
   data() {
     return {
       showBook: true,
       filter: true,
       instagram: "https://instagram.com/abam.afriansyah",
-      message: 'You loaded this page on ' + new Date().toLocaleString(),
+      message: "You loaded this page on " + new Date().toLocaleString(),
       books: [
         {
-            title: "Bincang Akhlak",
-            author: "Ustad Jek",
-            img: "/assets/1.jpg",
-            fav: true,
+          title: "Bincang Akhlak",
+          author: "Ustad Jek",
+          img: "/assets/1.jpg",
+          fav: true
         },
         {
-            title: "Ubur Ubur Lembur",
-            author: "Raditya Dika",
-            img: "/assets/2.jpg",
-            fav: false,
+          title: "Ubur Ubur Lembur",
+          author: "Raditya Dika",
+          img: "/assets/2.jpg",
+          fav: false
         },
         {
-            title: "This is Why I Need You",
-            author: "Brian Khrisna",
-            img: "/assets/3.jpg",
-            fav: true,
-        },
+          title: "This is Why I Need You",
+          author: "Brian Khrisna",
+          img: "/assets/3.jpg",
+          fav: true
+        }
       ],
       page: 321,
-    }
+      modal: {
+        title: "This is a Modal",
+        text: "Ini adalah sebagian text dari isi data"
+      }
+    };
   },
   methods: {
     reverseHandling() {
-        this.page++
+      this.page++;
     },
     togleBook() {
-        this.showBook = !this.showBook
+      this.showBook = !this.showBook;
     },
     togleFav(e) {
-        this.books[e].fav = !this.books[e].fav
+      this.books[e].fav = !this.books[e].fav;
     },
     togleFilterFav() {
-        this.filter = !this.filter
+      this.filter = !this.filter;
     }
   },
   computed: {
     filteredBooks() {
-        return this.books.filter((book) => book.fav)
+      return this.books.filter(book => book.fav);
     }
   }
-}
+};
 </script>
 
-<style>
-  .container {
-    margin: 1rem;
-    padding: 1rem;
-    border: solid 1px;
-    border-radius: 10px;
-  }
+<style scoped>
+.container {
+  margin: 1rem;
+  padding: 1rem;
+  border: solid 1px;
+  border-radius: 10px;
+}
 
-  #data span {
-    display: block;
-  }
+#data span {
+  display: block;
+}
 
-  .box {
-    text-align: center;
-    background: #ddd;
-    width: 200px;
-    padding: 100px 0;
-    margin: 20px;
-    display: inline-block;
-  }
+.box {
+  text-align: center;
+  background: #ddd;
+  width: 200px;
+  padding: 100px 0;
+  margin: 20px;
+  display: inline-block;
+}
 
-  .list-book {
-    list-style: none;
-    padding: 0;
-    width: auto;
-  }
+.list-book {
+  list-style: none;
+  padding: 0;
+  width: auto;
+}
 
-  .list-book li {
-    background-color: lightgrey;
-    margin: 1rem;
-    padding: 1rem;
-    /* border: solid 1px black; */
-    border-radius: 10px;
-    box-shadow: 5px 5px 5px #ddd;
-    display: flex ;
-  }
+.list-book li {
+  background-color: lightgrey;
+  margin: 1rem;
+  padding: 1rem;
+  /* border: solid 1px black; */
+  border-radius: 10px;
+  box-shadow: 5px 5px 5px #ddd;
+  display: flex;
+}
 
-  .list-book li.fav {
-    color: midnightblue;
-    background-color: lightpink;
-  }
+.list-book li.fav {
+  color: midnightblue;
+  background-color: lightpink;
+}
 
+.list-book li img {
+  padding: 0px 1rem;
+}
 
-  .list-book li img {
-    padding: 0px 1rem;
-  }
+h1 {
+  text-align: center;
+}
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,30 +1,41 @@
 <template>
   <div class="backdrop">
     <div class="modal">
-      <p>This is a Modal</p>
+      <h1>{{ modalData.title }}</h1>
+      <p>{{ text }}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Modal"
+  name: "Modal",
+  props: ["modalData", "text"]
+  // methods: {
+  //   getData() {
+  //     console.log(this.modalData);
+  //   }
+  // }
 };
 </script>
 
 <style>
-.modal {
-  width: 400px;
-  padding: 20px;
-  margin: 100px auto;
-  background: white;
-  border-radius: 10px;
-}
 .backdrop {
   top: 0;
   position: fixed;
   background: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100%;
+}
+.modal {
+  text-align: center;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.modal h1 {
+  color: blue;
 }
 </style>
